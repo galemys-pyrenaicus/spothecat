caaf368822c8c3fb7fa74aa664a93e167f538d8d
Galemys <desman.bzh@gmail.com>
2020-08-29 23:53:29 +0300

    Server POC app is ready galemys-pyrenaicus/spotecat#1

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import time
 import traceback
 import logging
 import configparser
+import os
+import sys
 
 config = configparser.ConfigParser()
 config.read('spothecat.conf')
@@ -16,33 +18,68 @@ dbcred = config['DATABASE']
 ffoncred = config['PHONE']
 gmapcred = config['GMAP']
 
-obtain_location() #Comment out to test without network connection and copy loca.sample to loca
+def check_availiability(host, retrying=3):
+    for i in range(retrying):
+        if os.system("ping -c 1 " + host+" > /dev/null") == 0:
+            return True
+        print("Testing connection")
+        time.sleep(20)
+    else:
+        return False
 
-connlocadb = psycopg2.connect(dbname=dbcred['dbname'], user=dbcred['dbuser'], password=dbcred['dbpass'], host=dbcred['dbhost'])
-cursor = connlocadb.cursor()
+def obtain_location(oncemore=False):
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname=ffoncred['ip'], port=int(ffoncred['port']), key_filename='/home/desman/.ssh/id_rsa', banner_timeout=300)
+        stdin, stdout, stderr = client.exec_command('sh /data/data/com.termux/files/home/scrt/myloc.sh')
+        time.sleep(20)
+        sftp = client.open_sftp()
+        remotepath = '/data/data/com.termux/files/home/storage/loca'
+        localpath = 'loca'
+        sftp.get(remotepath, localpath)
+        if os.path.getsize('loca') == 1:
+            raise FileNotFoundError;
+        client.close()
+    except paramiko.ssh_exception.AuthenticationException:
+        print("Auth error while connecting to %s", (ffoncred['ip']))
+    except paramiko.ssh_exception.BadAuthenticationType:
+        print("Auth type error while connecting to %s , check config", (ffoncred['ip']))
+    except paramiko.ssh_exception.SSHException:
+        print("Error connecting phone")
+    except FileNotFoundError:
+        if oncemore == False:
+            print("There is a problem with location file, retrying once more in 30s")
+            time.sleep(30)
+            obtain_location(True)
+        else:
+            print("There is a problem with location data, probably gps signal lost")
+            sys.exit()
 
-with open('loca', "r") as read_file:
-    loca = json.load(read_file, strict=False)
-    query = "INSERT INTO location_parsed (lat, lon, tme, prv, spd) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING"
-    cursor.execute(query, (loca['latitude'], loca['longitude'], loca['date'], loca['provider'], loca['speed']))
-    connlocadb.commit()
+def draw_map():
+    connlocadb = psycopg2.connect(dbname=dbcred['dbname'], user=dbcred['dbuser'], password=dbcred['dbpass'], host=dbcred['dbhost'])
+    cursor = connlocadb.cursor()
+    gmap = gmplot.GoogleMapPlotter(60.000000, 30.000000, 10, apikey=gmapcred['apikey'])
 
-gmap = gmplot.GoogleMapPlotter(60.000000, 30.000000, 10, apikey=gmapcred['apikey'])
+    try:
+        with open('loca', "r") as read_file:
+            loca = json.load(read_file, strict=False)
+            query = "INSERT INTO location_parsed (lat, lon, tme, prv, spd) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING"
+            cursor.execute(query, (loca['latitude'], loca['longitude'], loca['date'], loca['provider'], loca['speed']))
+            connlocadb.commit()
+            cursor.execute('SELECT * FROM location_parsed')
+            queryres = cursor.fetchall()
+            for row in queryres:
+                gmap.marker(row[0],row[1], color='cornflowerblue', title=row[2])
+            gmap.draw('whereami_map.html')
+    except:
+        print("Exception with loca or db")
 
-cursor.execute('SELECT * FROM location_parsed')
-queryres = cursor.fetchall()
-for row in queryres:
-    gmap.marker(row[0],row[1], color='cornflowerblue', title=row[2])
-gmap.draw('whereami_map.html')
+def main():
+    if check_availiability(ffoncred['IP']) == True:
+        obtain_location() #Comment out to test without network connection and copy loca.sample to loca
+        draw_map()
+    else:
+        print ("Can't reach host")
 
-def obtain_location():
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=ffoncred['ip'], port=int(ffoncred['port']), key_filename='/home/desman/.ssh/id_rsa', banner_timeout=300)
-    stdin, stdout, stderr = client.exec_command('sh /data/data/com.termux/files/home/scrt/myloc.sh')
-    time.sleep(15)
-    sftp = client.open_sftp()
-    remotepath = '/data/data/com.termux/files/home/storage/loca'
-    localpath = 'loca'
-    ftp.get(remotepath, localpath)
-    client.close()
+main()
