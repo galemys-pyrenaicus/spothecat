6b5afdfa4a62f5ede5aa9e34674f0c7e0c0bfbb5
Galemys <desman.bzh@gmail.com>
2021-01-09 01:23:35 +0300

    Setup script added

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if VERB="$( which apt )" 2> /dev/null; then
+  apt install git python3 postgresql
+elif VERB="$( which yum )" 2> /dev/null; then
+  yum install git python3 postgresql
+fi
+cd /opt
+git clone https://github.com/galemys-pyrenaicus/spothecat
+mkdir /etc/spothecat
+mv /opt/spothecat/spothecat.conf.sample /etc/spothecat/spothecat.conf.sample
+pip3 install Flask flask-login gmplot configparser
