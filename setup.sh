143c3c2741d97ba13f6f305e2043c7e631660d73
Galemys <desman.bzh@gmail.com>
2021-01-23 14:50:47 +0300

    Setup script changed galemys-pyrenaicus/spothecat#6

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ dbuser_pass=$(openssl rand -base64 12)
 if VERB="$( which apt )" 2> /dev/null; then
   sudo apt install git python3.6 postgresql93 postgresql93-contrib
 elif VERB="$( which yum )" 2> /dev/null; then
-  sudo dnf install git python3.6 glibc-locale-source glibc-langpack-ru
+  sudo dnf install -y git python3.6 glibc-locale-source glibc-langpack-ru
   sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
   sudo dnf -qy module disable postgresql
   sudo dnf install -y postgresql96-server
@@ -14,19 +14,23 @@ elif VERB="$( which yum )" 2> /dev/null; then
 fi
 cd /opt
 git clone https://github.com/galemys-pyrenaicus/spothecat
-mkdir /etc/spothecat
-mv /opt/spothecat/spothecat.conf.sample /etc/spothecat/spothecat.conf.sample
+sudo mkdir /etc/spothecat
+sudo mv /opt/spothecat/spothecat.conf.sample /etc/spothecat/spothecat.conf.sample
 pip3 install Flask flask-login gmplot configparser psycopg2-binary paramiko --user
-echo postgres:"$dbuser_pass" | chpasswd
-echo "$dbuser_pass" >> ~/.dbpasswd
-sed -E -i '/all/s/md5|peer|ident/trust/g' /var/lib/pgsql/9.6/data/pg_hba.conf
-sed -E -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/9.6/data/postgresql.conf
+#echo postgres:"$dbuser_pass" | chpasswd
+#echo "$dbuser_pass" >> ~/.dbpasswd
+sudo sed -E -i '/all/s/md5|peer|ident/trust/g' /var/lib/pgsql/9.6/data/pg_hba.conf
+sudo sed -E -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/9.6/data/postgresql.conf
+sudo systemctl restart postgresql-9.6
 psql -h localhost -U postgres -c "CREATE DATABASE location WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8' TABLESPACE = pg_default CONNECTION LIMIT = -1;"
 psql -h localhost -U postgres -c 'CREATE TABLE public.location_parsed (lat real NOT NULL, lon real NOT NULL, tme text COLLATE pg_catalog."default" NOT NULL, prv text COLLATE pg_catalog."default" NOT NULL, spd real NOT NULL, CONSTRAINT location_parsed_pkey PRIMARY KEY (tme)) TABLESPACE pg_default;'
-psql -h localhost -U postgres -c 'CREATE TABLE public.users (id integer NOT NULL, login text COLLATE pg_catalog."default" NOT NULL, pass_hash text COLLATE pg_catalog."default", role text COLLATE pg_catalog."default" NOT NULL, active boolean NOT NULL, address text COLLATE pg_catalog."default", connected text[] COLLATE pg_catalog."default", CONSTRAINT users_pkey PRIMARY KEY (id, login)) TABLESPACE pg_default;'
+psql -h localhost -U postgres -c 'CREATE TABLE public.users (login text COLLATE pg_catalog."default" NOT NULL, pass_hash text COLLATE pg_catalog."default", role text COLLATE pg_catalog."default" NOT NULL, active boolean NOT NULL, address text COLLATE pg_catalog."default", connected text[] COLLATE pg_catalog."default", CONSTRAINT users_pkey PRIMARY KEY (id, login)) TABLESPACE pg_default;'
 psql -h localhost -U postgres -c 'ALTER TABLE public.location_parsed OWNER to postgres;'
 psql -h localhost -U postgres -c 'ALTER TABLE public.users OWNER to postgres;'
-sudo echo "[Unit]
+psql -h localhost -U postgres -c "INSERT INTO users (login, pass_hash, role, active) VALUES ('admin', '8C6976E5B5410415BDE908BD4DEE15DFB167A9C873FC4BB8A81F6F2AB448A918', 'admin', 'True')"
+
+sudo cat > /lib/systemd/system/spothecat.service << EOL
+[Unit]
 Description=Spothecat App
 After=multi-user.target
 
@@ -36,7 +40,9 @@ ExecStart=/usr/bin/python3.6 /opt/spothecat/server.py
 StandartInput=tty-force
 
 [Install]
-WantedBy=multi-user.target" > /lib/systemd/system/spothecat.service
+WantedBy=multi-user.target
+EOL
+
 sudo systemctl daemon-reload
 sudo systemctl enable spothecat.service
 sudo systemctl start spothecat.service
